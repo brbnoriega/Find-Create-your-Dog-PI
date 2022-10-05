29be7a24bf3069241378174a14a1be8d3ced0ce0
brbnoriega <barbaranoriega330@gmail.com>
2022-10-05 14:07:50 -0300

    el buenardo de gabriel

--- api/index.js
+++ api/index.js
@@ -29,6 +29,6 @@ const { conn } = require('./src/db.js');
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3000"); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
